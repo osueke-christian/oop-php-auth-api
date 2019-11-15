bbdea4f61cd8eea71695e6f3798d3f3348ac8884
Christian Osueke <christian.osueke@vfdgroup.com>
2019-11-15 20:26:33 +0100

    Setup Base Model as Model to hold connection to be shared to other models, and also to implement utility methods for filtering fillable properties of a Database Model

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -8,4 +8,22 @@ use App\Traits\ResponseTrait;
 class Model{
     use ResponseTrait;
     
+    public $connection;
+
+    function __construct(){
+        $this->connection = (new Connection())->getConnection();
+    }
+
+
+    protected function filterArgs(Array $args){
+        // Check if fillables are set and if it was set as an arrays
+        if(!isset($this->fillables)) return $args;
+        if(!is_array($this->fillables)) throw new Exception('Fillables varaible must be an array');
+
+        $filtered = [];
+        foreach($args as $key=>$value){
+            if(in_array($key, $this->fillables)) $filtered[$key] = $value;
+        }
+        return $filtered;
+    }
 }
