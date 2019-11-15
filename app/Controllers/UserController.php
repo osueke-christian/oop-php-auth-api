447f6bae9471263b82c55adf8f51b9195a4690a3
Christian Osueke <christian.osueke@vfdgroup.com>
2019-11-15 20:30:49 +0100

    Added Implementation for getting logged in user and all registered users

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -5,5 +5,22 @@ namespace App\Controllers;
 use App\Models\User;
 
 class UserController extends BaseController{
-    //
+    private $user;
+
+    public function __construct(){
+        $this->user = new User();
+        // middlewares and guards can be implemented here
+    }
+
+    public function getUser($id){
+        $result = $this->user->find($id);
+        if(!$result) return $this->jsonResponse(404, 'Not found', $result);
+        return $this->jsonResponse(200, 'Success', $result);
+    }
+
+    public function getAllUsers(){
+        $result = $this->user->findAll();
+        return $this->jsonResponse(200, 'Success', $result);
+    }
+
 }
