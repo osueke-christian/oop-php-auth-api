c285a3e95a70ea1bf59ffb6b6287e42502ccb463
Christian Osueke <christian.osueke@vfdgroup.com>
2019-11-15 20:24:38 +0100

    Setup BaseController class

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -8,4 +8,17 @@ use App\Traits\ResponseTrait;
 class BaseController{
     use ResponseTrait;
 
+    protected function sanitizeInput(Array $input){
+        $input = $input;
+        foreach($input as $key=>$value){
+            $input[$key] = htmlspecialchars(strip_tags($value));
+        }
+        return $input;
+    }
+
+    public static function checkAuth(String $authHeader){ // Find a way to auto get the authheader
+        if(isset($_SESSION[$authHeader]) && $_SESSION[$authHeader] !== null) return $_SESSION[$authHeader];
+        else return false;
+    }
+
 }
